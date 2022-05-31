351227bd2d1c52113d974b0777008660d697260c
Eakarach Jansang <eakarach.jansang@student.vives.be>
2022-05-31 10:09:27 +0200

    Init

---
+++ EP.MAINAPP/ViewModels/ViewModelMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EP.DOMAIN;
+
+namespace EP.MAINAPP.ViewModels
+{
+    public class ViewModelMain : ViewModelBase
+    {
+        
+    }
+}
